e2f05e5748b436795929513c30a82a2d661e0233
Amalekites <darkmatter6212@gmail.com>
2026-03-22 18:40:03 +0300

    very last fix

--- Core/settings.py
+++ Core/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"] 
+CSRF_TRUSTED_ORIGINS=["myportfolio-production-75d1.up.railway.app"]
 
 
 # Application definition
